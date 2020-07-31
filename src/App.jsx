e0328baf8846d5de8df1ad88a010d9317ca284f8
Knob <nbr.41to@gmail.com>
2020-07-31 10:08:29 +0900

    第一段階完了！

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Link,
+  Redirect,
+  Auth,
+} from 'react-router-dom';
+// import Layout from './components/Layout'
+import Home from './pages/Home';
+import About from './pages/About';
+import Work from './pages/Work';
+import Contact from './pages/Contact';
 
-function App() {
+export default function App() {
   return (
-    <>
-
-    </>
+    <BrowserRouter> 
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route path="/work" component={Work} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
-export default App;
+// 課題1
+// react-router-tabs
+// react-tabs
+// リンク先に切り替えたときに,
+// 再レンダリングが起こり,
+// タッチエフェクトが表示されない
+
+// 課題2
+// Layoutは各ページか,App.jsxか,
+
