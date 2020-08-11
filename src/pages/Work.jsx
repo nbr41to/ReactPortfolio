5b1fe26535cffcf0e6e8953500f89abade0967d8
Knob <nbr.41to@gmail.com>
2020-08-11 10:29:34 +0900

    2020/08/11

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -5,8 +5,22 @@ export default function Work() {
     return (
         <>
             <h1>Work</h1>
+            <h2>Technology×Psychology</h2>
+            <p>ITと心理学を使って,多くの人が幸せに暮せる方法を追求しております.</p>
+            <ul>
+                <h2>具体的なできること</h2>
+                <li>ITコンサルティング</li>
+                <li>Webアプリケーション開発</li>
+                <li>アプリケーション開発</li>
+                <li>心理コンサルティング</li>
+                <li>コーチング</li>
+                <li>カウンセリング</li>
+                <li>成長メンター</li>
+                <li>学習メンター</li>
+                <li>数学講師（中学・高校）</li>
+                <li>数学講師（中学・高校）</li>
+            </ul>
             <p>仕事のほどんどは,PC,iPad,iPhoneで行っております.</p>
-
         </>
     );
 }
