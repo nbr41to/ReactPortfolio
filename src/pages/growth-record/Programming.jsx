e15891877ab070c7800500ca8210df3e366eefe9
nbr41to <nbr.41to@gmail.com>
2020-08-15 17:13:41 +0900

    ProgPortfolioの追加

--- src/pages/growth-record/Programming.jsx
+++ src/pages/growth-record/Programming.jsx
@@ -7,6 +7,7 @@ export default function Programing() {
         <div className="programming">
             <h1>Programming</h1>
             <h2>成長を記録するページです.</h2>
+            <a href="https://nobmath123.web.app/">数学学習Webアプリ</a>
         </div>
     );
 }
