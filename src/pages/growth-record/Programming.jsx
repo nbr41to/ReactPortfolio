47c18926385b43071baa44ff615e76b8b7aedd23
Knob <nbr.41to@gmail.com>
2020-08-15 17:31:12 +0900

    2020/08/15 更新

--- src/pages/growth-record/Programming.jsx
+++ src/pages/growth-record/Programming.jsx
@@ -7,6 +7,11 @@ export default function Programing() {
         <div className="programming">
             <h1>Programming</h1>
             <h2>成長を記録するページです.</h2>
+            <ul>
+                <h2>開発物一覧</h2>
+                <li><a href="https://nobmath123.web.app/">数学学習支援アプリ（個人開発）</a></li>
+                <li><a href="https://karaokensaku.web.app/">カラオケ検索(共同開発)</a></li>
+            </ul>
         </div>
     );
 }
